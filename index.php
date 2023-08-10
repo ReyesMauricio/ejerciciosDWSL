6bb39cb6fe9ebdda8bd95445516323be2c12e7e2
Mauricio Reyes <72460314+ReyesMauricio@users.noreply.github.com>
2023-08-09 23:41:02 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+/*Ejercicio 1*/
+/*Se asignan las variables con sus respectivos valores*/
+//-----------------------------------------------------------------------------------------------------
+// $salario = 566;
+
+// if($salario <= 450){
+//     $descuento = $salario *(0.08);
+//     $total = $salario - $descuento;
+//     echo "El descuento mensual del 8%, total descuento $$descuento, total credito: $$total <br/>";
+// }else if($salario <= 800){
+//     $descuento = $salario *(0.15);
+//     $total = $salario - $descuento;
+//     echo "El descuento mensual del 15%, total descuento $$descuento, total credito: $$total <br/>";
+// }else if($salario <= 800){
+//     $descuento = $salario *(0.18);
+//     $total = $salario - $descuento;
+//     echo "El descuento mensual del 18%, total descuento $$descuento, total credito: $$total <br/>";
+// }else{
+//     $descuento = $salario *(0.20);
+//     $total = $salario - $descuento;
+//     echo "El descuento mensual del 20%, total descuento $$descuento, total credito: $$total <br/>";
+// }
+//-----------------------------------------------------------------------------------------------------
+
+/*Ejercicio 2*/
+//-----------------------------------------------------------------------------------------------------
+// $precioProducto = 0.75;
+// $saldo = 5.00;
+
+// $suelto = $saldo-$precioProducto;
+// echo "Compra completa. Total ingresado: $saldo, cambio total: $suelto";
+//-----------------------------------------------------------------------------------------------------
+
+/*Ejercicio 3*/
+//-----------------------------------------------------------------------------------------------------
+// $numImagen = 6;
+//     for ($i = 1; $i <=$numImagen; $i++) {
+//         echo "<img src='./img/pokemon.gif' style='max-width:5%; padding:10px'>";
+//     }
+//-----------------------------------------------------------------------------------------------------
+
+/*Ejercicio 4*/
+//-----------------------------------------------------------------------------------------------------
+$PrecioDeJuego = 1;
+$CreditoInicial = 5;
+$BalanceDeCredito = $CreditoInicial;
+
+while ($BalanceDeCredito >= $PrecioDeJuego){
+    echo "Tu crédito actual es de: $" .$BalanceDeCredito. " Puedes Seguir jugando <br/><p>";
+    $BalanceDeCredito -= $PrecioDeJuego;
+}
+
+echo "Se agotaron tus creditos. ¡Recarga para seguir jugando.";
+
+
+//-----------------------------------------------------------------------------------------------------
+
+?>
